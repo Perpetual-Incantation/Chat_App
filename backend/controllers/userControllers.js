9270fc95c8b3acacc41961f8dd1ac9e543e2b902
4snehajain123 <sneha.jain@mvyapari.com>
2023-03-16 11:54:51 +0530

    initial

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,52 @@
+const asyncHandler = require("express-async-handler");
+const generateToken = require("../config/generateToken");
+const User = require("../Models/userModel");
+const registerUser = asyncHandler(async (req, res) => {
+  console.log(req.body);
+  const { name, email, password, pic } = req.body;
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("please enter details");
+  }
+  const userExist = await User.findOne({ email });
+  if (userExist) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+    pic,
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Failed to create the user");
+  }
+});
+
+const authUser = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid Email or Password");
+  }
+};
+module.exports = { registerUser, authUser };
---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
+const dotenv = require("dotenv");
+const express = require("express");
+const connectDB = require("./config/db");
+const { chats } = require("./data/data");
+const app = express();
+dotenv.config();
+var cors = require("cors");
+const { notFound, errorHandler } = require("./middlewares/errorMiddlware");
+app.use(cors());
+
+connectDB();
+
+app.use(express.json()); //to accept json data
+
+app.get("/", (req, res) => {
+  res.send("api is running");
+});
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+app.get("/api/chat/:id", (req, res) => {
+  //console.log(req.params.id);
+  const singleChat = chats.find((c) => c._id == req.params.id);
+  res.send(singleChat);
+});
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.use("/api/user", userRoutes);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server started on port ${PORT}`.yellow.bold));
